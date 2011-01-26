b0d0fc8f218485811c847ec163c9fba5a288eb6e
Antonio Touriño <atourino@gmail.com>
2011-01-26 17:35:26 -0500

    explicación de lo que hace el sitio.

--- application/views/index.php
+++ application/views/index.php
@@ -6,6 +6,12 @@
 			<li>Tienes tu cuenta de Twitter y eres un tweet-adicto.</li>
 			<li>Quieres mostrarles a todos tus <em>followers</em> el apoyo por tu equipo.</li>
 		</ul>
+		<h4>&iquest;Qu&eacute; hace?</h4>
+		<ul>
+			<li>Le agrega el log de tu equipo favorito a tu avatar en Twitter.</li>
+			<li>Si lo deseas, proclama tu pasi&oacute;n por tu equipo favorito por medio de un mensaje de Twitter.</li>
+			<li>Profit!</li>
+		</ul>
 		<?php echo anchor("app/index/sigin", '<img src="http://a0.twimg.com/images/dev/buttons/sign-in-with-twitter-d.png" alt="" />') ?>
 	</div>
 </div><!--#content-->
