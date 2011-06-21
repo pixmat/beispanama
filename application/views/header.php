e8b17c920c401b81062c3b74c5737d13b81817c2
Demostenes Garcia <demogar@Demostenes-Garcias-Mac.local>
2011-06-21 13:28:35 -0500

    changed the href on header github image

--- application/views/header.php
+++ application/views/header.php
@@ -22,6 +22,6 @@
 	</head>
 	<body>
 		
-		<a href="http://github.com/you"><img style="position: absolute; top: 0; right: 0; border: 0;" src="https://assets1.github.com/img/7afbc8b248c68eb468279e8c17986ad46549fb71?repo=&url=http%3A%2F%2Fs3.amazonaws.com%2Fgithub%2Fribbons%2Fforkme_right_darkblue_121621.png&path=" alt="Fork me on GitHub"></a>
+		<a href="http://github.com/demogar/beispanama"><img style="position: absolute; top: 0; right: 0; border: 0;" src="https://assets1.github.com/img/7afbc8b248c68eb468279e8c17986ad46549fb71?repo=&amp;url=http%3A%2F%2Fs3.amazonaws.com%2Fgithub%2Fribbons%2Fforkme_right_darkblue_121621.png&amp;path=" alt="Fork me on GitHub"></a>
 		
 		<h1><span>Beispanama: comparte el fervor por tu equipo de b&eacute;isbol en Twitter</span></h1>
